b19cf0c7a7b46c085b4eba6eaf825a6bdda5a7bd
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-05-04 11:05:20 -0400

    changed json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-export const App = () => {
+export default function App() {
   return (
     <div
       style={{
@@ -7,10 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
-};
+}
